28c4f9f660e0f2bfefcfe96aee1219df27c227f7
khanhNguyen <duykhanhlb4999@gmail.com>
2021-10-10 12:58:33 +0700

    first commit

---
+++ precache-manifest.b57b8c06a5d9400e5757ad2992391b48.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f2e67736ac6858f5c73c629d3d6822",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37c29983521fd6bef21e",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "23470ee13bee35f15ccf",
+    "url": "/static/css/main.1f08463c.chunk.css"
+  },
+  {
+    "revision": "37c29983521fd6bef21e",
+    "url": "/static/js/2.29916ffe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.29916ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23470ee13bee35f15ccf",
+    "url": "/static/js/main.4b995720.chunk.js"
+  },
+  {
+    "revision": "f438879c68bfc8864397",
+    "url": "/static/js/runtime-main.cc36b19e.js"
+  },
+  {
+    "revision": "6c8f0a0e377cfa70efcd35ff52893b17",
+    "url": "/static/media/HTML5_badge.6c8f0a0e.png"
+  },
+  {
+    "revision": "cb8c528c358d92ebc10382d57c83b0cf",
+    "url": "/static/media/avatar4.cb8c528c.png"
+  },
+  {
+    "revision": "9d80dafbab1425d29830ba803ee5eb82",
+    "url": "/static/media/aws.9d80dafb.jpg"
+  },
+  {
+    "revision": "04badecd2799ba76b599d038533ca32f",
+    "url": "/static/media/boostrap.04badecd.png"
+  },
+  {
+    "revision": "134c20fce1144d5bc15a081935448a62",
+    "url": "/static/media/flutter.134c20fc.png"
+  },
+  {
+    "revision": "07e2a28c5e0849cea95d5bc4b9c2fb1f",
+    "url": "/static/media/javascript-logo.07e2a28c.png"
+  },
+  {
+    "revision": "3df1cf88ff770f666a8da47dff278eb2",
+    "url": "/static/media/logo3.3df1cf88.png"
+  },
+  {
+    "revision": "9100f2eb47cae949fb1f01853263103c",
+    "url": "/static/media/nodejs.9100f2eb.png"
+  },
+  {
+    "revision": "8a70d5a8c61e64762da240c57750ba50",
+    "url": "/static/media/python.8a70d5a8.png"
+  },
+  {
+    "revision": "aa89cda633bda7ff59e4fad757afa7f6",
+    "url": "/static/media/react.aa89cda6.png"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "/static/media/redux.59c46753.png"
+  }
+]);
